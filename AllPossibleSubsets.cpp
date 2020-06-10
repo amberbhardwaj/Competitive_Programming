624f482217fb7084efabe9c62297769624aaec05
Amber Bhardwaj <amber.bhardwaj10@gmail.com>
2020-06-10 18:25:30 +0530

    Update AllPossibleSubsets.cpp

--- AllPossibleSubsets.cpp
+++ AllPossibleSubsets.cpp
@@ -1,5 +1,15 @@
 //Given a set of distinct integers, S, return all possible subsets.
 // https://www.educative.io/courses/grokking-the-coding-interview/gx2OqlvEnWG
+/*
+Example 1:
+
+Input: [1, 3]
+Output: [], [1], [3], [1,3]
+Example 2:
+
+Input: [1, 5, 3]
+Output: [], [1], [5], [3], [1,5], [1,3], [5,3], [1,5,3]
+*/
 
 vector<vector<int> > Solution::subsets(vector<int> &A) {
     vector<vector<int> > subsets;
